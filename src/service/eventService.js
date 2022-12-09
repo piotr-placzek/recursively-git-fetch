2a6482f678096999080a2e3fddbc81d513bad55e
Piotr Płaczek <piotr.placzek.91@gmail.com>
2022-12-09 22:46:54 +0100

    [issue-#4] fix listeners registration (#5)

--- src/service/eventService.js
+++ src/service/eventService.js
@@ -1,7 +1,46 @@
 'use strict';
 
-var EventEmitter = require('events');
+const EventEmitter = require('events');
+const events = require('../events');
+const gitJobFailureListener = require('../listener/gitJobFailureListener');
+const gitJobSuccessListener = require('../listener/gitJobSuccessListener');
+const gitJobSkipListener = require('../listener/gitJobSkipListener');
+const gitRecursiveJobFinishListener = require('../listener/gitRecursiveJobFinishListener');
+const gitRecursiveJobStartListener = require('../listener/gitRecursiveJobStartListener');
 
-var ee = new EventEmitter();
+const ee = new EventEmitter();
 
-module.exports = ee;
+/**
+ * @returns {Map}
+ */
+function listenersMap() {
+    const listenersMap = new Map();
+    listenersMap.set(events.GIT_JOB_SUCCESS, gitJobSuccessListener);
+    listenersMap.set(events.GIT_JOB_FAILURE, gitJobFailureListener);
+    listenersMap.set(events.GIT_JOB_SKIP, gitJobSkipListener);
+    listenersMap.set(events.GIT_RECURSIVE_JOB_FINISH, gitRecursiveJobFinishListener);
+    listenersMap.set(events.GIT_RECURSIVE_JOB_START, gitRecursiveJobStartListener);
+    return listenersMap;
+}
+
+function registerListeners() {
+    const map = listenersMap();
+    for (let [key, value] of map) {
+        ee.on(key, value);
+    }
+}
+
+function unregisterListeners() {
+    const map = listenersMap();
+    for (let [key, value] of map) {
+        ee.removeAllListeners(key, value);
+    }
+}
+
+module.exports = {
+    on: ee.on,
+    emit: ee.emit,
+    listenersMap,
+    registerListeners,
+    unregisterListeners,
+};
