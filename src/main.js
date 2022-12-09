6b587c0e3a1531009e7655e271e4e1b3f64e801c
Piotr Płaczek <piotr.placzek.91@gmail.com>
2022-12-09 21:43:52 +0000

    [issue-#4] fix listeners registration

--- src/main.js
+++ src/main.js
@@ -1,34 +1,9 @@
 'use strict';
 
 const { ROOT_DIR_PATH, SKIP_REPOSITORY_PATH_REGEX } = require('./config');
-const events = require('./events');
 const eventService = require('./service/eventService');
-const gitJobFailureListener = require('./listener/gitJobFailureListener');
-const gitJobSuccessListener = require('./listener/gitJobSuccessListener');
-const gitJobSkipListener = require('./listener/gitJobSkipListener');
-const gitRecursiveJobFinishListener = require('./listener/gitRecursiveJobFinishListener');
-const gitRecursiveJobStartListener = require('./listener/gitRecursiveJobStartListener');
 const gitService = require('./service/gitService');
 const loggerService = require('./service/loggerService');
-
-/**
- * @param {Funcion} fn
- */
-function listeners(fn) {
-    fn(events.GIT_JOB_SUCCESS, gitJobSuccessListener);
-    fn(events.GIT_JOB_FAILURE, gitJobFailureListener);
-    fn(events.GIT_JOB_SKIP, gitJobSkipListener);
-    fn(events.GIT_RECURSIVE_JOB_FINISH, gitRecursiveJobFinishListener);
-    fn(events.GIT_RECURSIVE_JOB_START, gitRecursiveJobStartListener);
-}
-
-function registerListeners() {
-    listeners(eventService.on);
-}
-
-function unregisterListeners() {
-    listeners(eventService.removeAllListeners);
-}
 
 /**
  * @param {Array<String} argv
@@ -39,7 +14,7 @@ async function main(argv) {
         skipRe: SKIP_REPOSITORY_PATH_REGEX,
     };
 
-    registerListeners();
+    eventService.registerListeners();
 
     for (let i = 0; i < rootDirectoriesPaths.length; ++i) {
         try {
@@ -51,7 +26,7 @@ async function main(argv) {
 }
 
 process.on('SIGTERM', () => {
-    unregisterListeners();
+    eventService.unregisterListeners();
 });
 
 module.exports = main;
